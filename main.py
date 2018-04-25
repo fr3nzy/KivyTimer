b5c9906950be7fbc76993cebf7d9ab4382b8ee1f
Clueless <bill.bernardot@gmail.com>
2018-04-25 18:35:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,35 +14,45 @@ Config.set('graphics', 'height', '600')
 
 
 class Timer:
+
 	def start(self):
 		self.app = App.get_running_app()
 		# countdown timer
-		self.time = self.app.root.ids.time.text
-		self.time_int = int(self.time[:2] + self.time[3:5] + self.time[6:8])
-		self.seconds = int(self.time[-2] + self.time[-1])
-		self.minutes = int(self.time[-5] + self.time[-4])
-		self.hours = int(self.time[0] + self.time[1])
+		self.time = self.app.root.ids.time.text # time label
+		self.hours = int(self.time[0] + self.time[1]) # xx:00:00
+		self.minutes = int(self.time[-5] + self.time[-4]) # 00:xx:00
+		self.seconds = int(self.time[-2] + self.time[-1])  # 00:00:xx
+		
+		total_seconds = (self.hours*60*60) + (self.minutes*60) + (self.seconds)
+		self.app.root.ids.timer_progress.max = total_seconds # max value for ProgressBar
 
-		#TODO TODO TODO TODO
-		while self.time_int is not 0:
-			if self.seconds > 0:
-				self.clock_event = Clock.schedule_interval(lambda dt: self.update_time(), 1)
-				self.time_int -= self.seconds
-			if self.minutes == 0 and self.hours > 0:
-				self.hours -= 1
-				self.minutes = 60
-			if self.seconds == 0 and self.minutes > 0:
-				self.seconds = 60
-				self.minutes -= 1
-				self.clock_event = Clock.schedule_interval(lambda dt: self.update_time(), 1)
-				self.time_int -= self.seconds
+		self.clock_event = Clock.schedule_interval(self.update_time, 1) # call every second
 				
-	def update_time(self): 
-		self.seconds -= 1 
-		self.time_int -= 1
-		self.app.root.ids.time.text = (self.time[:-2] + str(self.seconds))
+	def update_time(self, dt): 
 		if self.seconds == 0:
-			self.stop()
+			if self.minutes > 0:
+				self.minutes -= 1
+				self.seconds = 60
+			elif self.hours > 0:
+				self.hours -= 1
+				self.minutes = 59
+				self.seconds = 60
+			else: # if play is pressed when 00:00:00
+				self.app.root.ids.play_pause.source = 'img/play.png'
+				return self.stop()
+				
+		
+		self.seconds -= 1 
+		self.app.root.ids.timer_progress.value += 1
+		
+		def label_format(data): # add '0' before time if only 1 digit present (below 10)
+			return str(data) if len(str(data)) == 2 else '0' + str(data)
+		
+		# update label	
+		self.app.root.ids.time.text = label_format(self.hours) + ':' + label_format(self.minutes) + ':' + label_format(self.seconds)
+		
+		if self.seconds == 0 and self.minutes == 0 and self.hours == 0: # countdown ended
+			self.stop()	
 		
 	def stop(self):
 		self.clock_event.cancel()
@@ -70,12 +80,15 @@ class SetTime(Popup):
 	def btn_on_press(self, widget):
 		app = App.get_running_app()
 		app.root.ids.time.text = self.txt_input.text
+		app.root.ids.timer_progress.value = 0
 		self.close_popup()
 
 
 class ChangeButton(Button):
 	def on_release(self):
-		SetTime().load_popup()
+		app = App.get_running_app()
+		if app.root.ids.play_pause.source == 'img/play.png':
+			SetTime().load_popup()
 		
 		
 class PlayButton(Button, Timer):
@@ -89,10 +102,12 @@ class PlayButton(Button, Timer):
 			self.stop()
 			
 
-class ResetButton(Button):
+class ResetButton(Button, Timer):
 	def on_press(self):
 		app = App.get_running_app()
-		app.root.ids.time.text = '00:00:00'
+		if app.root.ids.play_pause.source == 'img/play.png':
+			app.root.ids.time.text = '00:00:00'
+			app.root.ids.timer_progress.value = 0
 
 
 class BreathTimer(App):
